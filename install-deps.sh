29865e1442cd5a823a00a4f0618dd3f57d0e2a26
Daniel Westendorf <daniel.westendorf@push-it-forward.com>
2013-10-31 08:35:52 -0500

    dw changes

--- install-deps.sh
+++ install-deps.sh
@@ -3,7 +3,6 @@
 
 # https://github.com/jamiew/git-friendly
 # the `push` command which copies the github compare URL to my clipboard is heaven
-sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
--- readme.md
+++ readme.md
@@ -16,10 +16,9 @@ I do something nice with my `PATH` there:
 
 ```shell
 # PATH like a bawss
-      PATH=/opt/local/bin
+PATH=/opt/local/bin
 PATH=$PATH:/opt/local/sbin
 PATH=$PATH:/bin
-PATH=$PATH:~/.rvm/bin
 PATH=$PATH:~/code/git-friendly
 # ...
 

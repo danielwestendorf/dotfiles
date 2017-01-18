d23ece7c10e953ab12292dd8a2aab6bdb27c06bc
Daniel Westendorf <daniel@prowestech.com>
2017-01-18 14:25:47 -0700

    Add quicklook plugins which are awesome

--- install-deps.sh
+++ install-deps.sh
@@ -9,3 +9,6 @@ brew install z
 
 # direnv
 brew instll direnv
+
+# Quicklook plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package quicklookase qlvideo
